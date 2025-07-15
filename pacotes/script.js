f5de9520429ffaecd7827a1e91258acdf038fee2
Fernando Pegoraro Bilia <fernandopbilia2304@gmail.com>
2025-07-15 19:17:06 -0300

    add uma pasta

---
+++ pacotes/script.js
@@ -0,0 +1,12 @@
+//Como descobrir os pacotes
+//npmjs.com 
+
+const now = dayjs()
+console.log(now.format("DD/M/YY"))
+console.log(now.format("DD/MM/YYYY"))
+console.log(now.format("YYYY-MM-DD"))
+console.log(now.format("DD/MM/YYYY HH:mm:ss"))
+
+const random = randomInt(1, 10)
+console.log(random)
+
